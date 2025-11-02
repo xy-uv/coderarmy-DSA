fc74bb4a62968e1c32f619c763889869df786407
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-02 12:41:04 +0600

    Add implementation for printing a square of column numbers based on user input in Q4.cpp

---
+++ Lectures/Lecture_006/Lecture_Code/Q4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,row,col;
+    cout<<"Enter the number of n: ";
+    cin>>n;
+
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n;col++){
+            cout<<col<<" ";
+        }cout<<endl;
+    }
+}
