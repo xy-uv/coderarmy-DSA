48c1d3fa2af47381be2c069b5541f21502f25255
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-02 20:33:30 +0600

    Add implementations for printing squares of numbers in 1.cpp and 2.cpp based on user input

---
+++ Lectures/Lecture_006/Home_Work/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,row,col;
+    cout<<"Enter the number: ";
+    cin>>n;
+    
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n;col++){
+            cout<<"11 ";
+        }cout<<endl;
+    }
+}
---
+++ Lectures/Lecture_006/Home_Work/2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row,col,n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n;col++){
+            cout<<col*col<<" ";
+        }cout<<endl;
+    }
+}
