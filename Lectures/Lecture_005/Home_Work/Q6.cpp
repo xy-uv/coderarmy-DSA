b47340e63bcacb9d3141b165522146e5cef5b43e
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-02 11:00:11 +0600

    Sum of n square and n cube natural number

---
+++ Lectures/Lecture_005/Home_Work/Q6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    long long int sum=0;
+    for(int i=1;i<=n;i++){
+        sum+=i*i*i;
+    }
+    cout<<"The sum is: "<<sum;
+}
