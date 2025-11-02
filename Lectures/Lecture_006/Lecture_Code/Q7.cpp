d7f0d67b0d54ff9339ad483c3a3f85c8cc9e7a88
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-11-02 16:25:04 +0600

    Alphabetic pattern

---
+++ Lectures/Lecture_006/Lecture_Code/Q7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row,col,n;
+    cout<<"Enter the Number: ";
+    cin>>n;
+
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n;col++){
+            char name='a'+(row-1);
+            cout<<name<<" ";
+        }cout<<endl;
+    }
+}
